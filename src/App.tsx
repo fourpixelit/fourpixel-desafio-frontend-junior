2db0a43e6df3cb8d37f356b407f0e26dad4d82f8
Daniel Mendes <daniel.s.m-@live.com>
2022-01-12 04:27:00 -0300

    clean up e uso do header

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,47 @@
+import React from "react";
 import "./App.css";
+import { Api } from "./lib/api";
+import { Header } from "./components/header/Header";
+import { IUser } from "./interfaces/IUser";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Seja bem-vindo ao nosso teste.</h1>
-    
-      <p>Antes de iniciar, leia o Readme com atenção. <br /> Qualquer dúvida, pergunte para os Recruiters, eles irão te auxiliar caso tenha algum problema</p>
+class App extends React.Component<
+  {},
+  { search: string; user: IUser | undefined }
+> {
+  constructor(props: {}) {
+    super(props);
 
-      <div>
-        <ul>
-          <li>📁 &nbsp;Crie a <b>estrutura</b> do projeto como desejar</li>
-          <li>🔗 &nbsp;Instale as <b>dependências</b> que achar necessário</li>
-          <li>🎨 &nbsp;Utilize sua <b>criatividade</b> com uma interface amigável</li>
-          <li>💡 &nbsp; Não basta ser bonito, precisa ser <b>usual</b></li>
-        </ul>
+    this.state = {
+      search: "",
+      user: undefined,
+    };
+  }
+
+  searchHandleChange = (event: { target: { value: string } }) => {
+    this.setState({ search: event.target.value });
+  };
+
+  searchHandleKeyPress = (event: { key: string }) => {
+    if (event.key !== "Enter") return;
+
+    console.log("buscar: " + this.state.search);
+    Api.get(`/users/${this.state.search}`).then(({ status, data }) => {
+      if (status === 200) this.setState({ user: data });
+      else console.error(status);
+    });
+  };
+
+  render() {
+    return (
+      <div className="fill-width fill-height">
+        <Header
+          search={this.state.search}
+          searchHandleChange={this.searchHandleChange}
+          searchHandleKeyPress={this.searchHandleKeyPress}
+        />
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
